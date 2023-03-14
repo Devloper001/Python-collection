67629b8f4888f42e69558e50189b5c85ea3eba0f
Dhrupad Pandya <43705389+Devloper001@users.noreply.github.com>
2023-03-14 19:09:01 +0530

    ok.

---
+++ app.py
@@ -0,0 +1,24 @@
+# GETTING STARTED!!!
+
+print("Hello World!!")
+print("*"*10)
+
+
+# // linting :  a process for identifying bugs and stylistic errors in your code //
+
+
+# // PYTHON IMPLEMENTATIONS: //
+# DEFAULT : CPYTHON
+# OTHERS: Jython(JAVA), Ironpython(C#), PyPy(Subset of Python)
+# USE: Allows other languages code implementation in Python
+
+
+# // HOW PYTHON CODE IS EXECUTED ? //
+# Python --> CPython --> Python ByteCode --> Python V.M --> Machine Code
+# Jython Execution : Phython --> Jython --> Java ByteCode --> Java V.M -->Machine Code
+
+
+# // QUIZZ //
+# Expression : Expression is a piece of code that produces a value (EX: "*" * 3 )
+# Syntax Error : A syntax error is kind of error due to bad syntax or bad grammar
+# What does Linter do? : A  linter is a tool that checks our code or potential error
