b18b70ab9c9c21ffe39f5c5e2ad02642ef5c8e04
Dhrupad Pandya <43705389+Devloper001@users.noreply.github.com>
2023-03-14 21:27:28 +0530

    ok.

--- Primitve Types/app.py
+++ Primitve Types/app.py
@@ -1,7 +1,9 @@
 # PRIMITIVE TYPES
 
-# // VARIABLES //
+
 import math
+
+# // VARIABLES //
 student_count = 100
 rating = 4.99
 is_published = True
@@ -15,16 +17,19 @@ course = "Python Programming"
 
 # // BUILT IN FUNCTIONS FOR STRING //
 print(len(course))
+# return letter of that position
 print(course[0])
 print(course[-1])
+# return letters inbetween these postions
 print(course[0:3])
+# return letter from beginning of string
 print(course[:3])
 
 
 # // ESCAPE SEQUENCES //
-# ESCAPE CHARACTER : \
-# HERE ESCAPE SEQUENCE IS : \"
-# FOR NEW LINE : \n
+# Escape Character  : \
+# Here Escape Sequence is : \"
+# For New Line : \n
 course = "Python \" Programming"
 print(course)
 
@@ -32,21 +37,32 @@ print(course)
 # // FORMATTED STRINGS //
 first = "Dhrupad"
 last = "Pandya"
+# a function which joins the strings together
 full = f"{first}middle{last}"
 print(full)
 
 
 # //STRING METHODS //
 courses = " Python programming "
+# in uppercase
 print(courses.upper())
+# in lowercase
 print(courses.lower())
+# capitalize first letter of title
 print(courses.title())
+# leading and trailing whitespaces gets removed
 print(courses.strip())
+# trailing whitespaces are removed
 print(courses.rstrip())
+# leading whitespaces are removed
 print(courses.lstrip())
+# for searching particular string
 print(courses.find("pro"))
+# for replacing particular string
 print(courses.replace("p", "j"))
+# return true or false
 print("pro" in courses)
+# return true or false
 print("swift" in courses)
 
 
@@ -73,3 +89,8 @@ print(round(2.9))
 print(abs(-2.9))
 print(math.ceil(2.2))
 # google python3 math modules
+
+
+# // Type Conversion
+x = input("x: ")
+y = x + 1
