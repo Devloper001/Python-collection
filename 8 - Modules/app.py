4748efac128055ea47e74f88c620c599a7214ce0
Dhrupad Pandya <43705389+Devloper001@users.noreply.github.com>
2023-03-20 00:22:54 +0530

    ok.

---
+++ 8 - Modules/app.py
@@ -0,0 +1,23 @@
+# // MODULES //
+
+
+# // CREATING MODULES //
+import sys
+import sales
+from sales import calc_shipping, calc_tax
+
+calc_shipping()
+calc_tax()
+
+# another way:
+# (import sales)
+sales.calc_shipping()
+
+
+# // COMPILED PYTHON FILES //
+# python creates combined version of file in .pyc format and is stored in form of bytecode.
+
+
+# // MODULE SEARCH PATH //
+# import sys
+print(sys.path)
